ac259c66a52057ab3c34345131a01df2d11b1ceb
nzhnvibec0der <nurzhansaruar@gmail.com>
2025-10-16 11:54:35 +0500

    Switch to PostgreSQL database on Render

--- digital_wardrobe/settings.py
+++ digital_wardrobe/settings.py
@@ -53,12 +53,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'digital_wardrobe.wsgi.application'
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
+
 
 AUTH_PASSWORD_VALIDATORS = []
 
