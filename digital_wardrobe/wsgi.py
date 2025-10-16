334e4c12382afb6d25b2950d911d00d7d4b053f5
nzhnvibec0der <nurzhansaruar@gmail.com>
2025-10-16 13:31:24 +0500

    Auto-create superuser on deploy

--- digital_wardrobe/wsgi.py
+++ digital_wardrobe/wsgi.py
@@ -16,3 +16,18 @@ except Exception as e:
     print(f"⚠️ Migration error: {e}")
 
 application = get_wsgi_application()
+
+import os
+from django.contrib.auth.models import User
+
+# --- Автоматическое создание суперпользователя при первом деплое ---
+try:
+    username = os.getenv("ADMIN_USER")
+    email = os.getenv("ADMIN_EMAIL")
+    password = os.getenv("ADMIN_PASSWORD")
+
+    if username and password and not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username=username, email=email, password=password)
+        print(f"✅ Superuser '{username}' created successfully on startup.")
+except Exception as e:
+    print(f"⚠️ Skipping admin auto-create: {e}")
